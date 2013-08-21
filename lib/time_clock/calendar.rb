84d57697a672aafb921039fd4606cb19ffe71e9f
barelyknown <sean@buytruckload.com>
2013-08-20 22:50:57 -0500

    use attr_reader for @shifts

--- lib/time_clock/calendar.rb
+++ lib/time_clock/calendar.rb
@@ -10,12 +10,12 @@ module TimeClock
 
     def add_shift(new_shift)
       starting_shift_count = @shifts.size
-      @shifts.each_with_index do |shift, index|
+      shifts.each_with_index do |shift, index|
         raise OverlappingShiftError if new_shift.overlaps?(shift)
-        break @shifts.insert(index, new_shift) if shift.start_time > new_shift.end_time
+        break shifts.insert(index, new_shift) if shift.start_time > new_shift.end_time
       end
-      @shifts << new_shift unless @shifts.size > starting_shift_count
-      @shifts
+      shifts << new_shift unless shifts.size > starting_shift_count
+      shifts
     end
 
   end
