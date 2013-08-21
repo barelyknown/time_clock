7a4fdc0fca35a0e4b553e16382de439962999a04
barelyknown <sean@buytruckload.com>
2013-08-20 22:48:03 -0500

    add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'simplecov'
+SimpleCov.start
+
 require 'time_clock'
+
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
